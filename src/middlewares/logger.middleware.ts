7c80841053f5d9d29ac9733075ed74f2ae35ca5c
osk2090 <osk2090@naver.com>
2022-08-28 21:21:30 +0900

    config & Logger

---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,19 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+  use(req: any, res: any, next: (error?: any) => void): any {
+    const { ip, method, originUrl } = req;
+    const userAgent = req.get('user-gent') || '';
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const contentLength = res.get('contents-length');
+      this.logger.log(
+        `${method} ${originUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
+      );
+    });
+    next();
+  }
+}
